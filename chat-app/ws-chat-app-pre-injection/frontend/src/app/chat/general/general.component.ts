f72ee444a74e4b330a09fba0e90d61426dd3fccc
Thomas Lhomme <lhomme.thomas@gmail.com>
2024-09-18 16:00:22 +0700

    coding content of general and music component

--- chat-app/ws-chat-app-pre-injection/frontend/src/app/chat/general/general.component.ts
+++ chat-app/ws-chat-app-pre-injection/frontend/src/app/chat/general/general.component.ts
@@ -2,6 +2,8 @@ import {Component, inject, OnInit} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {Message} from '../../../../../shared/model/user.message';
 import {CardModule} from 'primeng/card';
+import { WsService } from '../../ws.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-general',
@@ -14,15 +16,23 @@ import {CardModule} from 'primeng/card';
   styleUrl: './general.component.scss'
 })
 export class GeneralComponent{
+  private ws = inject(WsService)
+  private ws$!: Observable<any>
+
   messages: Message[] = [];
   username!:string;
   message: any;
 
   startChat(): void {
-
+    this.ws.username = this.username
+    this.ws$ = this.ws.getWs()
+    this.ws$.subscribe(message => {
+      this.messages.push(message)
+    })
   }
 
   sendMessage() {
+    this.ws.sendMessage(this.message)
     this.message = ''
   }
 
--- chat-app/ws-chat-app-pre-injection/frontend/src/app/chat/music/music.component.ts
+++ chat-app/ws-chat-app-pre-injection/frontend/src/app/chat/music/music.component.ts
@@ -2,6 +2,9 @@ import {Component, ElementRef, inject, Injector, input, viewChild} from '@angula
 import {Message} from '../../../../../shared/model/user.message';
 import {FormsModule} from '@angular/forms';
 import {CardModule} from 'primeng/card';
+import {CHANNEL, WsService } from '../../ws.service';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-music',
@@ -11,21 +14,31 @@ import {CardModule} from 'primeng/card';
     CardModule
   ],
   providers:[
+    WsService,
+    {provide: CHANNEL, useValue:'MUSIC'}
   ],
   templateUrl: './music.component.html',
   styleUrl: './music.component.scss'
 })
 export class MusicComponent {
+
+  private ws = inject(WsService)
+  private ws$!: Observable<any>
+
   messages: Message[] = [];
   username!:string;
-
   message: any;
 
   startChat(): void {
-
+    this.ws.username = this.username
+    this.ws$ = this.ws.getWs()
+    this.ws$.subscribe(message => {
+      this.messages.push(message)
+    })
   }
 
   sendMessage() {
+    this.ws.sendMessage(this.message)
     this.message = ''
   }
 
