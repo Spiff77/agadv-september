9930329fd7cebd8c6bced96257ff03fd0316a585
Thomas Lhomme <lhomme.thomas@gmail.com>
2024-09-19 14:46:13 +0700

    pwa

--- sandbox-16/src/app/app.module.ts
+++ sandbox-16/src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import {RouterModule, Routes} from '@angular/router';
 import {MenuComponent} from './menu/menu.component';
 import {NameService} from './name.service';
-import {BModule} from './b/b.module';
 
 const routes: Routes = [
   {path:'a', loadChildren: () => import('./a/a.module').then(m => m.AModule)},
